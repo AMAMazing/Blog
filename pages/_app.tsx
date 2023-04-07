d91cb6cdfd05edbd05ce5171f334d65f3d4984e0
AMA Mazing <alexmalone489@gmail.com>
2023-04-07 21:13:34 +1000

    Analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Header from "../components/Header.js";
+import{ Analytics } from'@vercel/analytics/react';
 
 
 export default function App({ Component, pageProps }: AppProps) {
@@ -8,6 +9,7 @@ export default function App({ Component, pageProps }: AppProps) {
     <>
     <Header/>
       <Component {...pageProps} />
+    <Analytics />
     </>
   )
 }
